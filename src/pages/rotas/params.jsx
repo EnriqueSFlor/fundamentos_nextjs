b32fbbcf82d6a731da05e1df6c4d8b9d98d6083f
EnriqueSFlor <enriquee.flor@gmail.com>
2022-01-19 22:19:44 -0300

    aprendendo rotas

---
+++ src/pages/rotas/params.jsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router"
+import Link from "next/link"
+
+export default function params(){
+
+    const router = useRouter()
+
+    const id = router.query.id
+    const nome = router.query.nome
+    
+
+
+    return(
+        <div>
+            <h1>id:{id}</h1>
+            <h1>Nome:{nome}</h1>
+            <Link href="/rotas">
+                <button>Voltar</button>
+            </Link>
+        </div>
+    )
+}
